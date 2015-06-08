3e475ef19ec52e6d1db771189ee22440311e251a
George Papaefthymiou <george.yord@gmail.com>
2015-06-08 17:26:21 +0300

    HAproxy creates different listeners depending on frontend/backend/domain

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ from subprocess import call
 import signal
 import sys
 import time
+from datetime import datetime
 
 env = Environment(loader=PackageLoader('haproxy', 'templates'))
+admin_user="stats"
+admin_password="stats"
 POLL_TIMEOUT=5
 
 signal.signal(signal.SIGCHLD, signal.SIG_IGN)
@@ -35,44 +38,60 @@ def get_services():
 
     host, port = get_etcd_addr()
     client = etcd.Client(host=host, port=int(port))
-    backends = client.read('/backends', recursive = True)
+    etcd_services = client.read('/services', recursive = True)
     services = {}
 
-    for i in backends.children:
+    for s in etcd_services.children:
 
-        if i.key[1:].count("/") != 2:
+        if s.key[1:].count("/") != 3:
             continue
 
-        ignore, service, container = i.key[1:].split("/")
-        endpoints = services.setdefault(service, dict(port="", backends=[]))
-        if container == "port":
-            endpoints["port"] = i.value
-            continue
-        endpoints["backends"].append(dict(name=container, addr=i.value))
+        ignore, name, port, container = s.key[1:].split("/")
+
+        endpoints = services.setdefault(port, dict())
+        domains = endpoints.setdefault(name, [])
+        domains.append(dict(ID=container, addr=s.value))
+
     return services
 
 def generate_config(services):
     template = env.get_template('haproxy.cfg.tmpl')
-    with open("/etc/haproxy.cfg", "w") as f:
-        f.write(template.render(services=services))
+    with open("/etc/haproxy/haproxy.cfg", "w") as f:
+        f.write(template.render(
+            services=services,
+            admin_user=admin_user,
+            admin_password=admin_password,
+            now=datetime.now().strftime("%d-%m-%y-%H-%M-%S")
+            ))
 
 if __name__ == "__main__":
     current_services = {}
+
+    if "ADMIN_USER" in os.environ:
+        admin_user = os.environ["ADMIN_USER"]
+
+    if "ADMIN_PASSWORD" in os.environ:
+        admin_password = os.environ["ADMIN_PASSWORD"]
+
     while True:
         try:
             services = get_services()
 
             if not services or services == current_services:
+                print "Config did not change. Sleep for " + str(POLL_TIMEOUT) + " sec"
                 time.sleep(POLL_TIMEOUT)
                 continue
 
-            print "config changed. reload haproxy"
+            print "Config changed, generating configs..."
             generate_config(services)
+            print "Reloading proxy..."
             ret = call(["./reload-haproxy.sh"])
             if ret != 0:
-                print "reloading haproxy returned: ", ret
+                print "Reloading haproxy returned: ", ret
                 time.sleep(POLL_TIMEOUT)
                 continue
+            else:
+                print "Proxy reloaded"
             current_services = services
 
         except Exception, e:
